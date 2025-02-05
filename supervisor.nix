27292dcdc7638bcabd5b0458f7067e8410967d7f
ARLJohnston <40921666+ARLJohnston@users.noreply.github.com>
2025-02-05 21:37:24 +0000

    Remove Grafana from nodes
    Remove Grafana from the supervisor nodes in favour of only running it
    locally as it is not required for stats collection and uses resources

--- supervisor.nix
+++ supervisor.nix
@@ -1,18 +1,9 @@
 { ... }:
 {
-  services.grafana = {
-    enable = true;
-    settings.server = {
-      domain = "grafana.arljohnston";
-      port = 2342;
-      addr = "127.0.0.1";
-      };
-  };
-
   services.prometheus = {
     enable = true;
     port = 9090;
     configText = builtins.readFile ./prometheus.yml;
-    extraFlags = ["--storage.tsdb.retention.time=1y"];
+    extraFlags = ["--storage.tsdb.retention.time=1y" "--storage.local.path=/"];
   };
 }
